98902b9fa8f72d643f8c4ad51cc04fceb1898595
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2024-12-03 12:51:32 +0200

    fix

--- zoo-merch/src/store/reducers/cartReducer.js
+++ zoo-merch/src/store/reducers/cartReducer.js
@@ -1,19 +1,27 @@
-import { getUserIdFromToken } from "../actions/cartActions";
+import { getTokenPayload, getUserIdFromToken } from "../actions/cartActions";
 
 const initialState = {
-  items: JSON.parse(localStorage.getItem(`cart_${getUserIdFromToken()}`)) || [],
+  items: [],
 };
 
 const cartReducer = (state = initialState, action) => {
   let updatedItems;
   const userId = getUserIdFromToken();
+  const encodedPayload = getTokenPayload();
 
-  if (!userId) return state;
+  if (!encodedPayload || !userId) return state;
+
+  const cartKey = `cart_${encodedPayload}`;
+
+  const storedCart = localStorage.getItem(cartKey);
+  if (storedCart) {
+    state.items = JSON.parse(storedCart);
+  }
 
   switch (action.type) {
     case "ADD_TO_CART":
       updatedItems = [...state.items, action.payload];
-      localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedItems));
+      localStorage.setItem(cartKey, JSON.stringify(updatedItems));
       return { ...state, items: updatedItems };
 
     case "UPDATE_ITEM_QUANTITY":
@@ -22,7 +30,7 @@ const cartReducer = (state = initialState, action) => {
           ? { ...item, quantity: action.payload.quantity }
           : item
       );
-      localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedItems));
+      localStorage.setItem(cartKey, JSON.stringify(updatedItems));
       return { ...state, items: updatedItems };
 
     case "REMOVE_FROM_CART":
@@ -30,11 +38,11 @@ const cartReducer = (state = initialState, action) => {
         (item) =>
           !(item.id === action.payload.id && item.size === action.payload.size)
       );
-      localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedItems));
+      localStorage.setItem(cartKey, JSON.stringify(updatedItems));
       return { ...state, items: updatedItems };
 
     case "CLEAR_CART":
-      localStorage.removeItem(`cart_${userId}`);
+      localStorage.removeItem(cartKey);
       return { ...state, items: [] };
 
     default:
